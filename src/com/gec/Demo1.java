845dd1d49ce948b4432f03bb2a0aa7699db1fa48
zzw <1498723062@qq.com>
2020-08-14 19:30:36 +0800

    第一个提交

--- src/com/gec/Demo1.java
+++ src/com/gec/Demo1.java
@@ -1,7 +1,11 @@
 package com.gec;
 
-public interface Demo1 {
+public class Demo1 {
     public static void main(String[] args) {
         System.out.println("git练习");
+    }
+
+    public void two(){
+        System.out.println("第二次提交");
     }
 }
